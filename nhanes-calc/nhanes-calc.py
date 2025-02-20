651199195584a3df9ae8e56f1ff4e64095877115
Anthony Khayat <anthonykhayat1@gmail.com>
2025-02-20 15:58:14 -0800

    Grabbing patient sequence numbers instead of aggregating the data

--- nhanes-calc/nhanes-calc.py
+++ nhanes-calc/nhanes-calc.py
@@ -1,43 +1,63 @@
 # Script to calculate the count and percent of total for each value
-# in the NHANES database when given a table and column name
+# in the NHANES database when given a table and column 
+import logging
 import psycopg2
 from psycopg2 import sql
 from decouple import config
 
-queries = {
+QUERIES = {
     "calc_exists": sql.SQL("SELECT COUNT(*) FROM pool.nhanes_data_pool WHERE ref_table ILIKE %s AND ref_column ILIKE %s "),
 
     "calc_insert": sql.SQL("INSERT INTO pool.nhanes_data_pool (value, count, total, ref_table, ref_column, field_name) "
-        "SELECT {column}, count(*), (SELECT count(*) FROM \"Translated\".{table}), {table_name}, {column_name}, \"SasLabel\" "
-        "FROM \"Translated\".{table} INNER JOIN \"Metadata\".\"QuestionnaireVariables\" ON (\"Variable\" ILIKE {column_name} AND \"TableName\" ILIKE {table_name}) "
-        "group by {column}, \"SasLabel\"")
+        "SELECT {column}, count(*), (SELECT count(*) FROM \"Translated\".{table}), {table_name}, {column_name}, {sas_label} "
+        "FROM \"Translated\".{table} group by {column}"),
+
+    "get_variables": "SELECT \"TableName\", \"Variable\", \"SasLabel\" FROM \"Metadata\".\"QuestionnaireVariables\" "
+         "WHERE \"Variable\" NOT LIKE 'SEQN' AND \"SasLabel\" IS NOT NULL LIMIT %s OFFSET %s"
 }
 
-def nhanes_calc(table, column):
+SELECT_LIMIT = 50
+
+def nhanes_calc():
     try:
         connection = psycopg2.connect(dbname=config('DB_NAME'), user=config('DB_USER'), host=config('DB_HOST'), password=config('DB_PASS'))
     except:
-      print(f"Unable to connect to database {config('DB_NAME')}")
+      logging.error(f"Unable to connect to database {config('DB_NAME')}")
 
     with connection.cursor() as cursor:
-        if nhanes_calc_exists(cursor, table, column):
-            print(f"Skipping calculation for {table}.{column} as it already exists")
-        else:
-            nhanes_calc_insert(cursor, table, column)
-            connection.commit()
-
+        offset = 0
+        variables = nhanes_get_variables(cursor, SELECT_LIMIT, offset) 
+        while len(variables) > 0:
+            for variable in variables:
+                table = variable[0]
+                column = variable[1]
+                sas_label = variable[2]
+                if nhanes_calc_exists(cursor, table, column):
+                    logging.info(f"Skipping calculation for {table}.{column} as it already exists")
+                else:
+                    nhanes_calc_insert(cursor, table, column, sas_label)
+                    connection.commit()
+            offset += SELECT_LIMIT
+            variables = nhanes_get_variables(cursor, SELECT_LIMIT, offset)
 
 def nhanes_calc_exists(cursor, table, column):
-    cursor.execute(queries['calc_exists'], [table, column])
+    cursor.execute(QUERIES['calc_exists'], [table, column])
     total = cursor.fetchone()[0]
     return total > 0
 
 
-def nhanes_calc_insert(cursor, table, column):
-    cursor.execute(queries['calc_insert'].format(column=sql.Identifier(column), table=sql.Identifier(table), 
-            column_name=sql.Literal(column), table_name=sql.Literal(table)))
-    print(f"{cursor.rowcount} rows migrated from {table}.{column}")
+def nhanes_calc_insert(cursor, table, column, sas_label):
+    try: 
+        cursor.execute(QUERIES['calc_insert'].format(column=sql.Identifier(column), table=sql.Identifier(table), 
+                column_name=sql.Literal(column), table_name=sql.Literal(table), sas_label=sql.Literal(sas_label)))
+        logging.debug(f"{cursor.rowcount} rows migrated from {table}.{column}")
+    except Exception as e:
+        logging.error(f"Error migrating {table}.{column}: {e}")
+
+def nhanes_get_variables(cursor, limit, offset):
+    cursor.execute(QUERIES['get_variables'], [limit, offset])
+    return cursor.fetchall()
 
 if __name__ == '__main__':
-    nhanes_calc('DEMO_L','DMDMARTZ')
-    nhanes_calc('DEMO_L','RIAGENDR')
+    logging.basicConfig(level=logging.DEBUG)
+    nhanes_calc()
--- sql/nhanes_data_pool.sql
+++ sql/nhanes_data_pool.sql
@@ -1,15 +1,29 @@
-CREATE TABLE pool.nhanes_data_pool (
-id BIGSERIAL PRIMARY KEY, 
-value VARCHAR, 
-count INTEGER NOT NULL, 
-total INTEGER NOT NULL, 
-percent NUMERIC GENERATED ALWAYS AS (ROUND((count::NUMERIC / total::NUMERIC) * 100, 2)) STORED, 
-ref_table VARCHAR NOT NULL, 
-ref_column VARCHAR NOT NULL,
-field_name VARCHAR NOT NULL
+CREATE TABLE pool.nhanes_patient_seqn (
+    seqn INT PRIMARY KEY,
+    table_suffix VARCHAR(10)
 );
 
-INSERT INTO nhanes_data_pool (value, count, total, ref_table, ref_column, field_name) 
-SELECT ":column", count(*), (SELECT count(*) FROM "Translated".":table"), ':table', ':column', "SasLabel" 
-FROM "Translated".":table" INNER JOIN "Metadata"."QuestionnaireVariables" ON ("Variable" ILIKE ':column' AND "TableName" ILIKE ':table')  
-group by ":column", "SasLabel";
+INSERT INTO pool.nhanes_patient_seqn
+SELECT DISTINCT seqn, suffix FROM (
+    SELECT a."SEQN" AS seqn, NULL AS suffix FROM "DEMO" a
+    UNION
+    SELECT b."SEQN" AS seqn, '_B' AS suffix from "DEMO_B" b
+    UNION
+    SELECT c."SEQN" AS seqn, '_C' AS suffix from "DEMO_C" c
+    UNION
+    SELECT d."SEQN" AS seqn, '_D' AS suffix from "DEMO_D" d
+    UNION
+    SELECT e."SEQN" AS seqn, '_E' AS suffix from "DEMO_E" e
+    UNION
+    SELECT f."SEQN" AS seqn, '_F' AS suffix from "DEMO_F" f
+    UNION
+    SELECT g."SEQN" AS seqn, '_G' AS suffix from "DEMO_G" g
+    UNION
+    SELECT h."SEQN" AS seqn, '_H' AS suffix from "DEMO_H" h
+    UNION
+    SELECT i."SEQN" AS seqn, '_I' AS suffix from "DEMO_I" i
+    UNION
+    SELECT j."SEQN" AS seqn, '_J' AS suffix from "DEMO_J" j
+    UNION
+    SELECT l."SEQN" AS seqn, '_L' AS suffix from "DEMO_L" l)
+ORDER BY seqn;
